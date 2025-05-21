f464f3e7d51300c4cea5a4a43175d393aac52c85
Pietro do Couto Freitas <pietrocoufre@gmail.com>
2025-05-21 18:57:59 -0300

    feat: location entity

---
+++ src/location/location.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class Location {
+  @PrimaryColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  state: string;
+
+  @Column()
+  cep: string;
+
+  @Column()
+  lat: number;
+
+  @Column()
+  long: number;
+}
