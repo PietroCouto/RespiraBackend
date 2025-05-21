f0137c6a627af81e7f494638e6ee32eb3f3bb629
Pietro do Couto Freitas <pietrocoufre@gmail.com>
2025-05-21 14:49:57 -0300

    Add TypeORM to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'respira1234',
+      database: 'respira-db',
+      entities: [],
+      synchronize: true, // Set to false in production
+      logging: true, // Enable logging for debugging
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {
+    // You can use the dataSource here if needed
+  }
+}
