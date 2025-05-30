cc526ae9b6de113d06d4285216c9389a861b41d5
Pietro do Couto Freitas <pietrocoufre@gmail.com>
2025-05-29 21:22:48 -0300

    fix: allow currentLocationAirHistory to have an empty report array

--- src/location/dto/currentLocationAirHistory.dto.ts
+++ src/location/dto/currentLocationAirHistory.dto.ts
@@ -1,9 +1,8 @@
-import { IsArray, IsNotEmptyObject, ValidateNested } from 'class-validator';
+import { IsArray, ValidateNested } from 'class-validator';
 import { AirQualityReportDto } from '../dto';
 import { Type } from 'class-transformer';
 
 export class currentLocationAirHistoryDto {
-  @IsNotEmptyObject()
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => AirQualityReportDto)
