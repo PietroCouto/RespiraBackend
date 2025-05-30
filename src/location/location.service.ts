f0400cafc1cfd9a9ba66fbbd29afe2651bab029b
Pietro do Couto Freitas <pietrocoufre@gmail.com>
2025-05-30 11:25:06 -0300

    feat: Improve getLatestLocationReport

--- src/location/location.service.ts
+++ src/location/location.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import { Location } from './entities';
 import {
   CurrentLocationAirQualityDto,
@@ -11,10 +10,7 @@ import { validateOrReject } from 'class-validator';
 
 @Injectable()
 export class LocationService {
-  constructor(
-    @InjectRepository(Location)
-    private dataSource: DataSource,
-  ) {}
+  constructor(private dataSource: DataSource) {}
 
   /**
    * Retrieves the latest air quality report for a specific location by name.
@@ -25,50 +21,72 @@ export class LocationService {
     name: string,
   ): Promise<CurrentLocationAirQualityDto> {
     // Query for the latest air quality report for the given location name
-    const result: CurrentLocationAirQualityDto[] = await this.dataSource.query(
-      `
-        SELECT
-          json_build_object(
-            'id', 		l.id,
-            'name', 	l.name,
-            'city', 	l.city,
-            'state',	l.state,
-            'cep', 	  l.cep,
-            'lat',    l.lat,
-            'long',   l.long 
-          ) AS "location",
+    const result: CurrentLocationAirQualityDto | undefined =
+      await this.dataSource
+        .createQueryBuilder(Location, 'l')
 
-          json_build_object(
-            'id', ar.id,
-            'date', ar.date,
-            'generalSeverity', ar."generalSeverity",
-            'pollutants',
-              json_agg(
-                json_build_object(
-                  'id', rp.id,
-                  'name', p.name,
-                  'concentration', rp.concentration,
-                  'severity', rp.severity
-                )
-              )
-        ) AS "airQualityReport"
-        FROM location l
-        INNER JOIN air_quality_sensor s ON s."locationId" = l.id
-        INNER JOIN air_quality_report ar ON ar."sensorId" = s.id
-        INNER JOIN report_pollutant rp ON rp."airQualityReportId" = ar.id
-        INNER JOIN pollutant p ON p.id = rp."pollutantId"
-        WHERE l.name = $1
-        GROUP BY
-        l.id, l.name, l.city, l.state, l.lat, l.long,
-        ar.id, ar.date, ar."generalSeverity"
-        ORDER BY ar.date DESC
-        LIMIT 1
-      `,
-      [name],
-    );
+        // Select the location
+        .select(
+          'json_build_object(' +
+            "'id'," +
+            'l.id,' +
+            "'name'," +
+            'l.name,' +
+            "'city'," +
+            'l.city,' +
+            "'state'," +
+            'l.state,' +
+            "'cep'," +
+            'l.cep,' +
+            "'lat'," +
+            'l.lat,' +
+            "'long'," +
+            'l.long' +
+            ')',
+          'location',
+        )
+
+        // Select the latest air quality report and pollutants as airQualityReport
+        .addSelect(
+          'json_build_object(' +
+            "'id', ar.id," +
+            "'date', ar.date," +
+            "'generalSeverity', ar.generalSeverity," +
+            "'pollutants'," +
+            'json_agg(' +
+            'json_build_object(' +
+            "'id', rp.id," +
+            "'name', p.name," +
+            "'concentration', rp.concentration," +
+            "'severity', rp.severity" +
+            ')' +
+            ')' +
+            ')',
+          'airQualityReport',
+        )
+
+        // Join the necessary tables to get the latest report
+        .innerJoin('air_quality_sensor', 's', 's.locationId = l.id')
+        .innerJoin('air_quality_report', 'ar', 'ar.sensorId = s.id')
+        .innerJoin('report_pollutant', 'rp', 'rp.airQualityReportId = ar.id')
+        .innerJoin('pollutant', 'p', 'p.id = rp.pollutantId')
+
+        // Filter by the location name
+        .where('l.name = :name', { name })
+
+        // Group by the location and report fields to ensure we get the latest report
+        .groupBy(
+          'l.id, l.name, l.city, l.state, l.lat, l.long, ar.id, ar.date, ar."generalSeverity"',
+        )
+
+        // Order by the report date in descending order to get the latest report
+        .orderBy('ar.date', 'DESC')
+
+        // Limit to the latest report
+        .getRawOne();
 
     // If no results are found, return an empty DTO
-    if (!result[0]) result[0] = new CurrentLocationAirQualityDto();
+    if (!result) return new CurrentLocationAirQualityDto();
 
     // THIS IS ABSOLUTELY TEMPORARY
     // TODO: REMOVE THIS
@@ -81,7 +99,7 @@ export class LocationService {
     // Convert the result to the DTO
     const dto: CurrentLocationAirQualityDto = plainToInstance(
       CurrentLocationAirQualityDto,
-      result[0],
+      result,
     );
 
     // Validate the DTO
