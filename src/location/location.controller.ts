e32bc0311e2610504a55c97286cdde5bd13643a3
Pietro do Couto Freitas <pietrocoufre@gmail.com>
2025-05-30 11:36:06 -0300

    task: add comments to location controller

--- src/location/location.controller.ts
+++ src/location/location.controller.ts
@@ -9,6 +9,11 @@ import {
 export class LocationController {
   constructor(private locationService: LocationService) {}
 
+  /**
+   * This endpoint retrieves the latest air quality report for a given location.
+   * @param name The name of the location to get the latest air quality report for.
+   * @returns A promise that resolves to the latest air quality report for the specified location.
+   */
   @Get('/current-location-air-quality')
   getLatestLocationReport(
     @Query('name') name: string,
@@ -16,6 +21,11 @@ export class LocationController {
     return this.locationService.getLatestLocationReport(name);
   }
 
+  /**
+   * This endpoint retrieves the air quality history for a given location.
+   * @param id The ID of the location to get the air quality history for.
+   * @returns A promise that resolves to the air quality history for the specified location.
+   */
   @Get('/current-location-air-history')
   getLocationHistory(
     @Query('id') id: bigint,
