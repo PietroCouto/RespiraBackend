e45c1a87590dd3f954887676e87c85a078071d9a
Pietro Couto <81260587+PietroCouto@users.noreply.github.com>
2025-06-01 20:34:13 -0300

    feat/improve-location

--- src/location/dto/index.ts
+++ src/location/dto/index.ts
@@ -1,5 +1,5 @@
 import { LocationDto } from './location.dto';
-import { CurrentLocationAirQualityDto } from './current_location_air_quality.dto';
+import { CurrentLocationAirQualityDto } from './currentLocationAirQuality.dto';
 import { AirQualityReportDto } from './airQualityReport.dto';
 import { PollutantDto } from './pollutant.dto';
 import { currentLocationAirHistoryDto } from './currentLocationAirHistory.dto';
